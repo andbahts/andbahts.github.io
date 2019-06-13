639a3c5b9b488fd83b221618336e2f6b3afe656a
IKN <andbahts@gmail.com>
2019-06-13 18:04:04 +0300

    laskjf

---
+++ closure.js
@@ -0,0 +1,16 @@
+function wrapValue(n) {
+    return () => n;
+}
+
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+
+console.log(wrap1());
+console.log(wrap2());
+
+function multiplier(factor) {
+    return number => number * factor;
+}
+
+let twice = multiplier(2);
+console.log(twice(3));
